4f122b5f709700d79ebc05fc214b2da3c9a0dcc5
Gabriel Farias <gabrielfalves2003@gmail.com>
2025-02-05 10:47:05 -0300

    [add] improved theming

--- src/styles/themes.jsx
+++ src/styles/themes.jsx
@@ -1,6 +1,6 @@
 export const darkTheme = {
     colors: {
-        black: "#000",
+        primary: "#000",
         white: "#fff",
         purple: "#A94CFB",
         blue: "#5C24FF",
@@ -9,6 +9,19 @@ export const darkTheme = {
     gradients: {
         greenBlueGreen:
             "linear-gradient(90deg, #61FF31 0%, #70D27F 38.02%, #5C24FF 75.83%, #56D94F 100%)",
+    },
+    font: {
+        primary: "Poppins, sans-serif",
+        secondary: "Open Sans, sans-serif",
+        colors: {
+            primary: "#fff",
+        },
+        sizes: {
+            small: "1rem",
+            medium: "1.5rem",
+            large: "2rem",
+            extralarge: "3rem",
+        },
     },
 };
 
