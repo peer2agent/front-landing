345d9674f0ed3360e0b33ecd8aebd62ac678bf19
Gabriel Farias <gabrielfalves2003@gmail.com>
2025-02-06 09:55:25 -0300

    [add] div for spacing

---
+++ src/components/ui/Divider/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export default function Divider() {
+    return <Container />;
+}
---
+++ src/components/ui/Divider/styles.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 1px;
+    margin: 1rem 0;
+`;
