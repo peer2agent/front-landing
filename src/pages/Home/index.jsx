ce6c71995f3dfcb2112ebb51dc4c482c9523dd94
Gabriel Farias <gabrielfalves2003@gmail.com>
2025-02-06 20:16:02 -0300

    [add] new sections in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 import Main from "../../components/Main";
 import Partners from "../../components/Partners";
 import Info from "../../components/Info";
+import Faq from "../../components/FAQ";
 
 import Title from "../../components/ui/Title";
 import Divider from "../../components/ui/Divider";
@@ -26,6 +27,7 @@ export default function Home() {
                 imagePath={chart}
                 title="Investment-Like Expense Tracking"
                 description="Easily monitor your spending habits with detailed categorization and analysis, empowering you to make informed financial decisions and stay within your budget ."
+                reverse={true}
             />
 
             <Divider />
@@ -35,10 +37,13 @@ export default function Home() {
                 imagePath={portfolio}
                 title="Enjoy risk free investments!"
                 description="Our guiding principle is simple: We do what's best for you. We prioritize your financial well-being above all else. With our unwavering commitment to your success, trust FinTrack to be your steadfast partner on your journey towards financial empowerment ."
-                reverse={true}
             />
 
+            <Divider />
+
             <Title>FAQ's</Title>
+
+            <Faq />
         </Container>
     );
 }
